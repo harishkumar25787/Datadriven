f2a5ad295bec19e5086ee570709dab1d76b86665
hsholapur <harishkumar25787@gmail.com>
2018-10-25 18:37:17 +0530

    test suite

---
+++ Exceldriven/tets_inher.py
@@ -0,0 +1,19 @@
+
+from Exceldriven.DDT import test_ddt
+
+
+class test_inher(test_ddt):
+    def __init__(self):
+        print("ssss")
+
+
+    def test_hahah(self):
+
+        x = test_ddt.test_ewindow()
+
+
+
+        print(x)
+
+ff = test_inher()
+ff.test_hahah
