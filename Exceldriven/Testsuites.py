3944b72d4f58f586d331f058d70f7e049a9e3f4c
hsholapur <harishkumar25787@gmail.com>
2018-10-26 12:21:51 +0530

    test suite-updated

--- Exceldriven/Testsuites.py
+++ Exceldriven/Testsuites.py
@@ -1,13 +1,12 @@
 import sys
 import unittest
-from unittest import TestLoader,TestSuite,TextTestRunner
 
 from Exceldriven import multiplewindow
 from Exceldriven import Asserttitle
 
 
-tc1 = TestLoader().loadTestsFromTestCase(multiplewindow.test_multi)
-tc2 = TestLoader().loadTestsFromTestCase(Asserttitle.test_hero1)
+tc1 = unittest.TestLoader().loadTestsFromTestCase(multiplewindow.test_multi)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(Asserttitle.test_hero1)
 # tc1 = unittest.TestLoader().loadTestsFromTestCase(exceldata.test_hero)
 # tc2 = unittest.TestLoader().loadTestsFromTestCase(Asserttitle.test_hero1)
 #
@@ -15,8 +14,8 @@ tc2 = TestLoader().loadTestsFromTestCase(Asserttitle.test_hero1)
 # smoke = unittest.TestSuite([tc1,tc2 ])
 #
 # runner = unittest.TextTestRunner(verbosity=2 ).run(smoke)
-suite = TestSuite([tc2,tc1])
-runner = TextTestRunner()
-runner.run(suite)
+suite = unittest.TestSuite([tc2,tc1])
+runner = unittest.TextTestRunner(verbosity=2).run(suite)
+
 # if __name__ == '__main__':
 #     unittest.runner
