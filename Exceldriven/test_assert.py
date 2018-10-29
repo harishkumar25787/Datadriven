9053f2b9178ab69ebc1f38cb3dfd1b28be374040
hsholapur <harishkumar25787@gmail.com>
2018-10-29 19:50:33 +0530

    test suite-updated

--- Exceldriven/test_assert.py
+++ Exceldriven/test_assert.py
@@ -1,23 +1,14 @@
 import time,xlrd,unittest,os,pytest
 from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from ddt import ddt,data
 
-
-@ddt
 class test_hero1(unittest.TestCase):
 
     def setUp(self):
         driverlocation = "D:\\Pythondriver\\chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = driverlocation
         self.driver = webdriver.Chrome(driverlocation)
-    @pytest.mark.run(order=1)
+    @pytest.fixture()
     def test_forasserrt(self):
         driver = self.driver
         driver.maximize_window()
--- Exceldriven/test_mulitplewindow.py
+++ Exceldriven/test_mulitplewindow.py
@@ -1,30 +1,17 @@
-import time,xlrd,unittest,os,pytest
-
+import time,unittest,os,pytest
 from pytest import skip
 from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from ddt import ddt,data,unpack
-
-
-@ddt
 
 class test_multi(unittest.TestCase):
-
-
-
 
 
     def setUp(self):
         driverlocation = "D:\\Pythondriver\\chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = driverlocation
         self.driver = webdriver.Chrome(driverlocation)
-    @pytest.mark.run
+
+    @pytest.fixture()
     def test_multipl_ewindow(self):
         driver = self.driver
         driver.maximize_window()
